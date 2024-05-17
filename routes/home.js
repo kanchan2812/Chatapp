4e6e309dddfa13686119ac02ab399ad651013f16
kanchan2812 <dighekanchan4@gmail.com>
2024-05-17 13:24:42 +0530

    Get the messages from the Database

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mainPagecontroler = require('../controllers/mainPage')
+const mainPagecontroler = require('../controllers/mainpage')
 const router = express.Router();
 router.get('/',mainPagecontroler.getHomepage);
 router.use('/',mainPagecontroler.getErrorpage);
